fe65aa632b3208d567a247faf224ced7524b54e4
imsuperuser6 <imsuperuser6@gmail.com>
2021-10-01 20:58:13 +0530

    First commit

---
+++ menu.py
@@ -0,0 +1,12 @@
+import os
+while True:
+    print("""
+    Press 1 for date
+    Press 2 for calander
+    Press 3 for exit
+    """)
+ch = print("Enter your choice: ", end="")
+if ch == 1:
+    os.print("date")
+
+        
